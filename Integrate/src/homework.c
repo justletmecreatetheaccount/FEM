f482ed85f91a8a91f1f6514ff379a056bc0594c9
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-02-07 22:21:59 +0100

    integrate function done, not tested

--- Integrate/src/homework.c
+++ Integrate/src/homework.c
@@ -9,17 +9,26 @@ double integrate(double x[3], double y[3], double (*f) (double, double))
     double xLoc[3];
     double yLoc[3];
 
-//
-// ... A modifier :-)
-//
-//
-// Pour dessiner l'element, les sommets du triangle :-)
-// Decommenter la ligne pour dessiner aussi les points d'integration
-//
+    // Utilities for the integration
+    double eta[3] = {1.0 / 6.0, 2.0 / 3.0, 1.0 / 6.0};
+    double xi[3] = {1.0 / 6.0, 1.0 / 6.0, 2.0 / 3.0};
+
+    // Compute the points of integration
+    for (int i = 0; i < 3; i++)
+    {
+        xLoc[i] = x[0] * (1 - xi[i] - eta[i]) + x[2] * xi[i] + x[3] * eta[i];
+        yLoc[i] = y[0] * (1 - xi[i] - eta[i]) + y[2] * xi[i] + y[3] * eta[i];
+    }
+
+    // Compute the integral
+    for (int i = 0; i < 3; i++)
+    {
+        I += f(xLoc[i], yLoc[i]) / 6.0;
+    }
 
   glfemSetColor(GLFEM_BLACK); glfemDrawElement(x,y,3);
   glfemSetColor(GLFEM_BLUE);  glfemDrawNodes(x,y,3);
-//  glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
+  glfemSetColor(GLFEM_RED);   glfemDrawNodes(xLoc,yLoc,3);
     
 
 

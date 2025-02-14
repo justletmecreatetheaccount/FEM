ae0353877a5bd4be2badbea5e0b4d07f9c546eda
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-02-14 14:15:57 +0100

    forme faite, merci emc

--- GeoMesh/src/homework.c
+++ GeoMesh/src/homework.c
@@ -64,19 +64,20 @@ void geoMeshGenerate() {
 //
  
     int ierr;
-    int idPlate = gmshModelOccAddRectangle(___, ___, ___, ___, ___, ___, ___,&ierr);   
+    int idPlate = gmshModelOccAddRectangle(-w/2.0, -h/2.0, 0.0, w, h, -1, 0.0, &ierr);
     ErrorGmsh(ierr);
-    int idNotch = gmshModelOccAddDisk(___, ___, ___, ___, ___, ___,NULL,0,NULL,0,&ierr); 
+    int idNotch = gmshModelOccAddDisk(x0, y0, 0.0, r0, r0, -1, NULL, 0, NULL, 0, &ierr); 
     ErrorGmsh(ierr);
-    int idHole  = gmshModelOccAddDisk(___, ___, ___, ___, ___, ___,NULL,0,NULL,0,&ierr);    
+    int idHole  = gmshModelOccAddDisk(x1, y1, 0.0, r1, r1, -1, NULL, 0, NULL, 0, &ierr);    
     ErrorGmsh(ierr);
     
-    int plate[] = {___,___};
-    int notch[] = {___,___};
-    int hole[]  = {___,___};
-    gmshModelOccCut(___,___,___,___,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
+    int plate[] = {2, idPlate};
+    int notch[] = {2, idNotch};
+    int hole[]  = {2, idHole};
+    
+    gmshModelOccCut(plate, 2, notch, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr); 
     ErrorGmsh(ierr);
-    gmshModelOccCut(___,___,___,___,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
+    gmshModelOccCut(plate, 2, hole, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr); 
     ErrorGmsh(ierr);
  
 //

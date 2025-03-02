6bced518f3a942f25b019d7e2fc5161e1f227e11
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-03-02 12:51:24 +0100

    revert copilot because predictibly he wrote trash

--- Poisson/src/homework.c
+++ Poisson/src/homework.c
@@ -67,7 +67,7 @@ void femPoissonFindBoundaryNodes(femPoissonProblem *theProblem)
 
 void femPoissonFree(femPoissonProblem *theProblem)
 {
-    
+
     // A completer :-)
 }
     
@@ -103,32 +103,8 @@ void femPoissonSolve(femPoissonProblem *theProblem)
     double x[4],y[4],phi[4],dphidxsi[4],dphideta[4],dphidx[4],dphidy[4];
     int iElem,iInteg,iEdge,i,j,map[4];
     int nLocal = theMesh->nLocalNode;
-
-    for (iElem = 0; iElem < theMesh->nElem; iElem++) {
-        femPoissonLocal(theProblem,iElem,map,x,y);
-        for (i = 0; i < theRule->n; i++) {
-            femDiscretePhi2(theSpace,theRule->xsi[i],theRule->eta[i],phi);
-            femDiscreteDphi2(theSpace,theRule->xsi[i],theRule->eta[i],dphidxsi,dphideta);
-            double detJ = 0;
-            for (j = 0; j < nLocal; j++) {
-                detJ += dphidxsi[j]*x[j] + dphideta[j]*y[j]; }
-            detJ = fabs(detJ);
-            for (i = 0; i < nLocal; i++) {
-                for (j = 0; j < nLocal; j++) {
-                    double Aij = 0;
-                    for (iInteg = 0; iInteg < theRule->n; iInteg++) {
-                        femDiscreteDphi2(theSpace,theRule->xsi[iInteg],theRule->eta[iInteg],dphidx,dphidy);
-                        Aij += (dphidx[i]*dphidx[j] + dphidy[i]*dphidy[j])*detJ*theRule->weight[iInteg]; }
-                    theSystem->A[map[i]][map[j]] += Aij; }
-                theSystem->B[map[i]] += 0; }
-        }
-    }
-    for (i = 0; i < theBoundary->nElem; i++) {
-        for (j = 0; j < nLocal; j++) {
-            theSystem->B[theBoundary->elem[i]] = 0; }
-    }
-    femFullSystemConstrain(theSystem,0,0);
-    femFullSystemEliminate(theSystem);
+    
+    
 }
 
 # endif

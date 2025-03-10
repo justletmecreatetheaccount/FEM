fdbe46d75bb0952a0246daa240defe7dcd272079
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-03-10 19:19:29 +0100

    it dont work

--- BandSolver/src/homework.c
+++ BandSolver/src/homework.c
@@ -5,9 +5,22 @@
 
 #ifndef NORENUMBER 
 
+double* assignableArrayPos;
+
+int compareNodePos(const void* firstNode, const void* secondNode) {
+    int* iOne = (int *) firstNode;
+    int* iTwo = (int *) secondNode;
+    double diff = assignableArrayPos[*iOne] - assignableArrayPos[*iTwo];
+    return (diff < 0) - (diff > 0);
+}
+
 void femMeshRenumber(femMesh *theMesh, femRenumType renumType)
 {
     int i;
+    int* toso = (int*) malloc(sizeof(int) * theMesh->nodes->nNodes);
+    for (i = 0; i < theMesh->nodes->nNodes; i++) {
+        toso[i] = i;
+    }
     
     switch (renumType) {
         case FEM_NO :
@@ -19,24 +32,53 @@ void femMeshRenumber(femMesh *theMesh, femRenumType renumType)
 // debut
 //
         case FEM_XNUM : 
+        assignableArrayPos = theMesh->nodes->X;
+        qsort(toso, theMesh->nodes->nNodes, sizeof(int), compareNodePos);
+        for (i = 0; i < theMesh->nodes->nNodes; i++) 
+                theMesh->nodes->number[toso[i]] = i;
+        break;
         case FEM_YNUM : 
-            for (i = 0; i < theMesh->nodes->nNodes; i++) 
-                theMesh->nodes->number[i] = i;
-            break;            
+        assignableArrayPos = theMesh->nodes->Y;
+        qsort(toso, theMesh->nodes->nNodes, sizeof(int), compareNodePos);
+        for (i = 0; i < theMesh->nodes->nNodes; i++) 
+                theMesh->nodes->number[toso[i]] = i;
+        break;            
 // 
 // end
 //
 
-        default : Error("Unexpected renumbering option"); }
+        default : Error("Unexpected renumbering option"); 
+        }
+    free(toso);
 }
+
 
 #endif
 #ifndef NOBAND 
 
 int femMeshComputeBand(femMesh *theMesh)
 {
-    int myBand = theMesh->nodes->nNodes;
-    return(myBand);
+    int bandwidth = 0;
+    int nElem = theMesh->nElem;
+    int nLocalNode = theMesh->nLocalNode;
+    int *elem = theMesh->elem;
+
+    // Loop over all elements
+    for (int e = 0; e < nElem; e++) {
+        // Loop over all nodes in the element
+        for (int i = 0; i < nLocalNode; i++) {
+            int node_i = elem[e * nLocalNode + i]; // Global index of node i
+            for (int j = 0; j < nLocalNode; j++) {
+                int node_j = elem[e * nLocalNode + j]; // Global index of node j
+                int diff = abs(node_i - node_j); // Difference in global indices
+                if (diff > bandwidth) {
+                    bandwidth = diff; // Update bandwidth if this difference is larger
+                }
+            }
+        }
+    }
+
+    return bandwidth + 1;
 }
 
 
@@ -46,7 +88,18 @@ int femMeshComputeBand(femMesh *theMesh)
 
 void femBandSystemAssemble(femBandSystem* myBandSystem, double *Aloc, double *Bloc, int *map, int nLoc)
 {
-    // A Ecrire :-)
+    for (int i = 0; i < nLoc; i++) {
+        int row = map[i];
+
+        for (int j = 0; j < nLoc; j++) {
+            int col = map[j];
+
+            if (row <= col) {
+                myBandSystem->A[row][col] += Aloc[i * nLoc + j];
+            }
+            myBandSystem->B[row] += Bloc[i];
+        }
+    }
 }
 
 
@@ -62,11 +115,35 @@ double  *femBandSystemEliminate(femBandSystem *myBand)
     B    = myBand->B;
     size = myBand->size;
     band = myBand->band;
-    
-    // A completer :-)
 
 
-    return(myBand->B);
+    /* Gauss elimination */
+
+    for (k=0; k < size; k++) {
+        if ( fabs(A[k][k]) <= 1e-16 ) {
+            printf("Pivot index %d  ",k);
+            printf("Pivot value %e  ",A[k][k]);
+            Error("Cannot eliminate with such a pivot"); }
+        jend = fmin(size,k + band);
+        for (i = k + 1 ; i <  jend; i++) {
+            factor = A[k][i] / A[k][k];
+            for (j = i ; j < jend; j++) 
+                A[i][j] = A[i][j] - A[k][j] * factor;
+            B[i] = B[i] - B[k] * factor; 
+        }
+    } //code de fem.c devoir 3
+
+    /* Back-substitution */
+
+    for (i = size - 1; i >= 0; i--) {
+        factor = 0;
+
+        for (j = i + 1; j < fmin(i + band, size); j++)
+            factor += A[i][j] * B[j];
+        B[i] = (B[i] - factor) / A[i][i];
+    }
+
+    return(myBand->B);//code de fem.c devoir 3
 }
 
 

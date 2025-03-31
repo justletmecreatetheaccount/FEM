2ecadcb7121c53db4b69954cc0c05f6eb5086e1d
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-03-31 22:10:46 +0200

    testing

--- fem-emc/project/homework.c
+++ fem-emc/project/homework.c
@@ -178,7 +178,8 @@ double *femElasticitySolve(femProblem *theProblem){
         }
     }
 
-    femConjugateGradient(theSystem);
+    femFullSystemEliminate(theSystem);
+    //femConjugateGradient(theSystem);
 
     for (int i = 0; i < theSystem->size; i++) {
         theProblem->soluce[i] = theSystem->B[i];

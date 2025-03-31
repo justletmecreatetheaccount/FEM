64a67819c472ea1571bc79d93d196510990c27f7
justletmecreatetheaccount <vladdoniga11@gmail.com>
2025-03-31 17:17:39 +0200

    adding reference files

--- GeoMesh/src/homework.c
+++ GeoMesh/src/homework.c
@@ -1,10 +1,18 @@
 #include "fem.h"
 
 
+/*
+#define chk(ierr)                                               \
+  if(ierr != 0){                                                \
+    fprintf(stderr, "Error on line %i in function '%s': "       \
+            "gmsh function returned non-zero error code: %i\n", \
+            __LINE__, __FUNCTION__, ierr);                      \
+    gmshFinalize(NULL);                                         \
+    exit(0);} // ierr  --- for ctest                             
+*/
 
 
-double geoSize(double x, double y){
-
+double geoSize(double x, double y) {
     femGeo* theGeometry = geoGetGeometry();
     
     double h = theGeometry->h;
@@ -13,28 +21,32 @@ double geoSize(double x, double y){
     double r0 = theGeometry->rNotch;
     double h0 = theGeometry->hNotch;
     double d0 = theGeometry->dNotch;
-  
     
     double x1 = theGeometry->xHole;
     double y1 = theGeometry->yHole;
     double r1 = theGeometry->rHole;
     double h1 = theGeometry->hHole;
     double d1 = theGeometry->dHole;
-
-
-//
-//     A modifier !
-//     
-// Your contribution starts here ....
-//
     
-     
-    return h;
-    
-//   
-// Your contribution ends here :-)
-//
+    double result = h;
 
+    // Getting the profile unidimensionally with d
+    double _d0 = sqrt(pow(x - x0, 2) + pow(y - y0, 2)) - r0; // Distance to the notch border
+    double _d1 = sqrt(pow(x - x1, 2) + pow(y - y1, 2)) - r1; // Distance to the hole border
+    if (_d0 <= d0) { // If the points are in the radius of r0 + d0 which is the transition zone
+        // Do Hermite Interpolation
+        double a2 = (3 * (h - h0)) / pow(d0, 2);
+        double a3 = (2 * (h0 - h)) / pow(d0, 3);
+        result = h0 + a2 * pow(_d0, 2) + a3 * pow(_d0, 3);
+    }
+    if (_d1 <= d1) { // Do not use elif in the case of the two transition zones overlapping
+        // Second Hermite Interpolation
+        double a2 = (3 * (h - h1)) / pow(d1, 2);
+        double a3 = (2 * (h1 - h)) / pow(d1, 3);
+        result = fmin(result,h1 + a2 * pow(_d1, 2) + a3 * pow(_d1, 3)); // Min for overlapping transition zones
+    }
+        
+    return result;
 }
 
 
@@ -46,7 +58,7 @@ void geoMeshGenerate() {
 
     double w = theGeometry->LxPlate;
     double h = theGeometry->LyPlate;
-     
+
     double x0 = theGeometry->xNotch;
     double y0 = theGeometry->yNotch;
     double r0 = theGeometry->rNotch;
@@ -57,32 +69,33 @@ void geoMeshGenerate() {
     double r1 = theGeometry->rHole;
  
 //
-//  -1- Construction de la géométrie avec OpenCascade
-//      On crée le rectangle
-//      On crée les deux cercles
+//  -1- Construction de la g�om�trie avec OpenCascade
+//      On cr�e le rectangle
+//      On cr�e les deux cercles
 //      On soustrait les cercles du rectangle :-)
 //
  
     int ierr;
-    int idPlate = gmshModelOccAddRectangle(___, ___, ___, ___, ___, ___, ___,&ierr);   
+    int idPlate = gmshModelOccAddRectangle(-w/2.0, -h/2.0, 0.0, w, h, -1, 0.0, &ierr);
     ErrorGmsh(ierr);
-    int idNotch = gmshModelOccAddDisk(___, ___, ___, ___, ___, ___,NULL,0,NULL,0,&ierr); 
+    int idNotch = gmshModelOccAddDisk(x0, y0, 0.0, r0, r0, -1, NULL, 0, NULL, 0, &ierr); 
     ErrorGmsh(ierr);
-    int idHole  = gmshModelOccAddDisk(___, ___, ___, ___, ___, ___,NULL,0,NULL,0,&ierr);    
+    int idHole  = gmshModelOccAddDisk(x1, y1, 0.0, r1, r1, -1, NULL, 0, NULL, 0, &ierr);    
     ErrorGmsh(ierr);
     
-    int plate[] = {___,___};
-    int notch[] = {___,___};
-    int hole[]  = {___,___};
-    gmshModelOccCut(___,___,___,___,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
+    int plate[] = {2, idPlate};
+    int notch[] = {2, idNotch};
+    int hole[]  = {2, idHole};
+    
+    gmshModelOccCut(plate, 2, notch, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr); 
     ErrorGmsh(ierr);
-    gmshModelOccCut(___,___,___,___,NULL,NULL,NULL,NULL,NULL,-1,1,1,&ierr); 
+    gmshModelOccCut(plate, 2, hole, 2, NULL, NULL, NULL, NULL, NULL, -1, 1, 1, &ierr); 
     ErrorGmsh(ierr);
  
 //
-//  -2- Définition de la fonction callback pour la taille de référence
+//  -2- D�finition de la fonction callback pour la taille de r�f�rence
 //      Synchronisation de OpenCascade avec gmsh
-//      Génération du maillage (avec l'option Mesh.SaveAll :-)
+//      G�n�ration du maillage (avec l'option Mesh.SaveAll :-)
                   
    
     geoSetSizeCallback(geoSize);
@@ -96,9 +109,9 @@ void geoMeshGenerate() {
 //
 //    gmshOptionSetNumber("Mesh.SaveAll", 1, &ierr);
 //    gmshOptionSetNumber("Mesh.RecombineAll", 1, &ierr);
-//    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);  chk(ierr);
-//    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);  chk(ierr);
-//    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  chk(ierr);
+//    gmshOptionSetNumber("Mesh.Algorithm", 8, &ierr);  //chk(ierr);
+//    gmshOptionSetNumber("Mesh.RecombinationAlgorithm", 1.0, &ierr);  //chk(ierr);
+//    gmshModelGeoMeshSetRecombine(2,1,45,&ierr);  //chk(ierr);
 //    gmshModelMeshGenerate(2, &ierr);  
    
  
@@ -106,7 +119,7 @@ void geoMeshGenerate() {
 //  Plot of Fltk
 //
 //   gmshFltkInitialize(&ierr);
-//   gmshFltkRun(&ierr);  chk(ierr);
+//   gmshFltkRun(&ierr);  //chk(ierr);
 //
     
 }

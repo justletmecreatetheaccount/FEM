1b2d4dcaa2399d82c01748ec135fe7bf8c79b6a8
kiwi <kiwi.bp.surviv@gmail.com>
2025-04-02 14:35:42 +0200

    improved version

--- fem-emc/genmesh/src/homework.c
+++ fem-emc/genmesh/src/homework.c
@@ -12,13 +12,60 @@
 */
 
 
-
-
 double geoSize(double x, double y)
 {
-    return 0.03;
- 
+    femGeo *theGeometry = geoGetGeometry();
+
+    double h = theGeometry->h;
+
+    double x0 = theGeometry->x_hole1 + 0.5 * theGeometry->w_hole1;  // center X
+    double y0 = theGeometry->y_hole1 + 0.5 * theGeometry->h_hole1;  // center Y
+    double halfW0 = 0.5 * theGeometry->w_hole1;
+    double halfH0 = 0.5 * theGeometry->h_hole1;
+    double d0 = 0.5;      // transition zone for hole 1
+    double h0 = 0.005;    // refined mesh size near hole 1
+
+    double x1 = theGeometry->x_hole2 + 0.5 * theGeometry->w_hole2;  // center X
+    double y1 = theGeometry->y_hole2 + 0.5 * theGeometry->h_hole2;  // center Y
+    double halfW1 = 0.5 * theGeometry->w_hole2;
+    double halfH1 = 0.5 * theGeometry->h_hole2;
+    double d1 = 0.5;      // transition zone for hole 2
+    double h1 = 0.01;     // refined mesh size near hole 2
+
+    // Default mesh size
+    double result = h;
+
+    double dx0 = fabs(x - x0) - halfW0;
+    double dy0 = fabs(y - y0) - halfH0;
+    if (dx0 < 0.0) dx0 = 0.0;
+    if (dy0 < 0.0) dy0 = 0.0;
+    double dist0 = sqrt(dx0 * dx0 + dy0 * dy0);
+
+    double dx1 = fabs(x - x1) - halfW1;
+    double dy1 = fabs(y - y1) - halfH1;
+    if (dx1 < 0.0) dx1 = 0.0;
+    if (dy1 < 0.0) dy1 = 0.0;
+    double dist1 = sqrt(dx1 * dx1 + dy1 * dy1);
+
+    if (dist0 <= d0) {
+        double a2 = (3.0 * (h - h0)) / (d0 * d0);
+        double a3 = (2.0 * (h0 - h)) / (d0 * d0 * d0);
+        double val0 = h0 + a2 * (dist0 * dist0) + a3 * (dist0 * dist0 * dist0);
+        result = val0 < result ? val0 : result;
+    }
+
+    if (dist1 <= d1) {
+        double a2 = (3.0 * (h - h1)) / (d1 * d1);
+        double a3 = (2.0 * (h1 - h)) / (d1 * d1 * d1);
+        double val1 = h1 + a2 * (dist1 * dist1) + a3 * (dist1 * dist1 * dist1);
+        result = val1 < result ? val1 : result;
+    }
+
+    return result;
 }
+
+
+
 void geoMeshGenerate() {
   femGeo *theGeometry = geoGetGeometry();
 

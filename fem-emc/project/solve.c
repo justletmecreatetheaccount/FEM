93d0d2aca590ee73aeea497eca7fdbc5681f70f7
kiwi <kiwi.bp.surviv@gmail.com>
2025-04-03 20:59:44 +0200

    boundary conditions

--- fem-emc/project/solve.c
+++ fem-emc/project/solve.c
@@ -1,36 +1,37 @@
-#include "../benchmark.h"
+#include "../main.h"
 #include "fem.h"
 #include <stdio.h>
 
-void elasticity_solve(const char *meshfile, const char *outfile, double E,
-                      double nu, double rho, double g)
+void elasticity_solve(const char *meshfile, const char *outfile)
 {
-  // Read the mesh and the problem
-  femGeo *theGeometry = geoMeshRead(meshfile);
-  femProblem *theProblem =
-      femElasticityCreate(theGeometry, E, nu, rho, g, PLANAR_STRAIN);
+    // Read the mesh and the problem
+    femGeo *theGeometry = geoMeshRead(meshfile);
+    femProblem *theProblem = femElasticityCreate(theGeometry, _E, _NU, _RHO, _G, PLANAR_STRAIN);
 
-  // Boundary conditions are Dirichlet X and Y
-  femElasticityAddBoundaryCondition(theProblem, "Plate left", DIRICHLET_X, 3 * 10e-7);
-  femElasticityAddBoundaryCondition(theProblem, "Plate right", DIRICHLET_X,
-                                    -3 * 10e-7);
-  femElasticityAddBoundaryCondition(theProblem, "Plate right", DIRICHLET_X,
-                                    -3 * 10e-7);
-  femElasticityAddBoundaryCondition(theProblem, "Plate bottom", DIRICHLET_Y,
-                                    0.0);
-  femElasticityAddBoundaryCondition(theProblem, "Plate bottom", DIRICHLET_X,
-                                    0.0);
+    // Boundary conditions
+    femElasticityAddBoundaryCondition(theProblem, "Plate left", DIRICHLET_X, 
+                                       _DISPLACEMENT);
+    femElasticityAddBoundaryCondition(theProblem, "Plate top-left", DIRICHLET_X,
+                                       _DISPLACEMENT);
+    femElasticityAddBoundaryCondition(theProblem, "Plate right", DIRICHLET_X,
+                                      -_DISPLACEMENT);
+    femElasticityAddBoundaryCondition(theProblem, "Plate top-right", DIRICHLET_X,
+                                      -_DISPLACEMENT);
+    femElasticityAddBoundaryCondition(theProblem, "Plate bottom", DIRICHLET_Y,
+                                      0.0);
+    femElasticityAddBoundaryCondition(theProblem, "Plate bottom", DIRICHLET_X,
+                                      0.0);
 
-  // Assemble and solve
-  // femElasticityPrint(theProblem);
-  double *theSoluce = femElasticitySolve(theProblem);
-  femElasticityForces(theProblem);
+    // Assemble and solve
+    // femElasticityPrint(theProblem);
+    double *theSoluce = femElasticitySolve(theProblem);
+    femElasticityForces(theProblem);
 
-  // Write out the solution
-  int nNodes = theGeometry->theNodes->nNodes;
-  femSolutionWrite(nNodes, 2, theSoluce, outfile);
+    // Write out the solution
+    int nNodes = theGeometry->theNodes->nNodes;
+    femSolutionWrite(nNodes, 2, theSoluce, outfile);
 
-  // free the allocated ressources
-  femElasticityFree(theProblem);
-  geoFinalize(theGeometry);
+    // free the allocated ressources
+    femElasticityFree(theProblem);
+    geoFinalize(theGeometry);
 }

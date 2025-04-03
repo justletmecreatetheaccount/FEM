4c24f657f4a9d7af50e9de4725ff8738b85d27a5
kiwi <kiwi.bp.surviv@gmail.com>
2025-04-03 20:59:03 +0200

    changing to main

---
+++ fem-emc/main.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define _DISPLACEMENT (3.0 * 10e-7)
+#define _E (211.0 * 10e9)
+#define _NU (0.3)
+#define _RHO (7.85 * 10e3)
+#define _G (9.81)
+
+void elasticity_solve(const char *meshfile, const char *outfile);
+
+#ifdef __cplusplus
+}
+#endif // C++
